3ae72175620695c38c9771704fbe8118ec3131b4
Breno <br3eno14@gmail.com>
2018-05-25 22:14:27 -0300

    Metodo de Criação de Cliente e Listagem de cliente

--- src/java/entities/Cliente.java
+++ src/java/entities/Cliente.java
@@ -1,8 +1,82 @@
 package entities;
 
-
-
+/**
+ *
+ * @author Breno
+ */
 
 public class Cliente {
+    private String nome;
+    private String sobrenome;
+    private String apelido;
+    private String data_nascimento;
+    private String registro;
+    private String doc_unico;
+    private String sexo;
+    private String pessoa;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public String getApelido() {
+        return apelido;
+    }
+
+    public void setApelido(String apelido) {
+        this.apelido = apelido;
+    }
+
+    public String getData_nascimento() {
+        return data_nascimento;
+    }
+
+    public void setData_nascimento(String data_nascimento) {
+        this.data_nascimento = data_nascimento;
+    }
+
+    public String getRegistro() {
+        return registro;
+    }
+
+    public void setRegistro(String registro) {
+        this.registro = registro;
+    }
+
+    public String getDoc_unico() {
+        return doc_unico;
+    }
+
+    public void setDoc_unico(String doc_unico) {
+        this.doc_unico = doc_unico;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getPessoa() {
+        return pessoa;
+    }
+
+    public void setPessoa(String pessoa) {
+        this.pessoa = pessoa;
+    }
     
 }
